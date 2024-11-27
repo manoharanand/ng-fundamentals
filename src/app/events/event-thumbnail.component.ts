b4ba4fbfd0497cbfbdaedf8fcfa1a904a3c9f332
Manohar Anand <1997manohar5@gmail.com>
2024-11-27 22:22:04 +0530

    Exploring *ngFor, *ngSwitch directives

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,9 +3,31 @@ import { Component , Input, Output, EventEmitter} from "@angular/core";
 //Write basic componet code first
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: './events-list.component.html',
+    template: `
+        <div class="well hoverwell thumbnail"> 
+        <h1> {{event.name}}</h1>
+            <div>Date: {{event.date}}</div>
+            <div [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'"> (Early start) </span>
+                <span *ngSwitchCase="'10:00 am'"> (Late start) </span>
+                <span *ngSwitchDefault> (Normal start) </span>
+            </div>
+            <div>Price: \${{event.price}}</div>
+            <div *ngIf="event?.location">
+                <span> Location: {{event.location.address}}</span>
+                <span class="pad-left">{{event.location.city}}, 
+                    {{event.location.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
+            </div>
+        </div>
+    `,
     styles: [`
-            .pad-left {margin-let: 10px},
+            .thumbnail {min-height: 210px; }
+            .pad-left {margin-let: 10px; },
+            .well div { color: #bbb; }
         `]
 })
 
